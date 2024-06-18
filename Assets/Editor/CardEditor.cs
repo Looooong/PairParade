c4a56a887f7ebcd2ffae411111496dac4d5d716e
Nguyễn Đức Long <duclong120995@gmail.com>
2024-06-19 06:02:42 +0700

    [Feature] Added Card, card sprites and card data

---
+++ Assets/Editor/CardEditor.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace PairParade.Editor {
+  public static class CardEditor {
+    [MenuItem("Pair Parade/Convert selected Sprites to Cards")]
+    static void ConvertSelectedSpritesToCard() {
+      var folder = EditorUtility.SaveFolderPanel("Choose folder to save the cards", "Assets", null);
+
+      if (string.IsNullOrEmpty(folder)) return;
+
+      var sprites = GetSpritesFromSelection().ToList();
+
+      try {
+        for (var i = 0; i < sprites.Count; i++) {
+          var sprite = sprites[i];
+          EditorUtility.DisplayProgressBar($"Converting... ({i + 1}/{sprites.Count})", sprite.name, (i + 1f) / sprites.Count);
+
+          var card = ScriptableObject.CreateInstance<Card>();
+          card.frontFace = sprite;
+
+          var relativePath = Path.GetRelativePath(Application.dataPath, folder);
+          AssetDatabase.CreateAsset(card, Path.Combine("Assets", relativePath, $"{sprite.name}.asset"));
+        }
+      } finally {
+        EditorUtility.ClearProgressBar();
+      }
+    }
+
+    [MenuItem("Pair Parade/Convert selected Sprites to Cards", true)]
+    static bool ValidateConvertSelectedSpritesToCard() => GetSpritesFromSelection().Any();
+
+    static IEnumerable<Sprite> GetSpritesFromSelection() => Selection.objects.OfType<Sprite>();
+  }
+}
---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace PairParade {
+  [CreateAssetMenu]
+  public class Card : ScriptableObject {
+    public Sprite frontFace;
+    public Sprite backFace;
+  }
+}
